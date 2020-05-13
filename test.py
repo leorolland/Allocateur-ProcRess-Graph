21580233012efcf7d4b85d2b4cb3f17bfa9371d9
Léo <rolland.leo@orange.fr>
2020-05-13 11:44:55 +0200

    First commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.Graph()
+
+G.add_node(1)
+G.add_nodes_from([2, 3, 4, 5])
+G.add_edges_from([(4, 5)])
+
+nx.draw(G, with_labels=True)
+plt.show()
