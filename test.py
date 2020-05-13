14c544b2ede68da1a6ed897605db0a86ae5ed93b
maelLV <mlevot99@gmail.com>
2020-05-13 16:00:10 +0200

    rien

--- test.py
+++ test.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 
+
 while (True):
 	nom = input("Ajouter un noeud (entrez un nombre): ")
 	# Ajout du noeud
-	G.add_node(noeud)
-	
-    # Ajout arc 
-    G = addArc(G)
+	G.add_node(nom)
+
 
 	# Mise a jour de l'affichage
 	plt.clf() # Nettoyage du plot
 	nx.draw(G, with_labels=True) # Dessin du nouveau plot
 	plt.pause(0.01) # Affichage non bloquant
+
