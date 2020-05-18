cf40c5fbf71557e60ea47bc454f18d3793069860
maelLV <mlevot99@gmail.com>
2020-05-18 11:46:17 +0200

    modif affichage

--- src/Affichage.py
+++ src/Affichage.py
@@ -3,24 +3,30 @@ import matplotlib.pyplot as plt
 
 class Affichage(object):
 
-    #Allocateur
-    allocateur = None
+	#Allocateur
+	allocateur = None
 
-    #Graphe
-    G = nx.Graph()
+	#Graphe
+	G = nx.Graph()
 
-    def __init__(self, all:Allocateur):
-        self.allocateur = all
+	def __init__(self, all):
+		self.allocateur = all
 
-    def addProcessus(self):
-        for p in self.allocateur.processus:
-            G.add_node(p.getName())
-    
-    def affichageGlobal(self):
-        # Mise a jour de l'affichage
-	    plt.clf() # Nettoyage du plot
-	    nx.draw(G, with_labels=True) # Dessin du nouveau plot
-	    plt.pause(0.01) # Affichage non bloquant
-        
+	def addProcessus(self):
+		for p in self.allocateur.processus:
+			self.G.add_node(p.getName())
 
-    
+	def addRessources(self):
+		for r in self.allocateur.ressources:
+			self.G.add_node(r.getName())
+	
+	def affichageGlobal(self):
+		# Mise a jour de l'affichage
+		self.G.clear()
+		self.addProcessus()
+		plt.clf() # Nettoyage du plot
+		nx.draw(self.G, with_labels=True) # Dessin du nouveau plot
+		plt.pause(0.01) # Affichage non bloquant
+		
+
+	
